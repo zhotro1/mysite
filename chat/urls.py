2405dfd216eddf24a724cc98559608c62315491b
NgocHieu <zhotro1@gmail.com>
2020-08-05 20:48:58 +0700

    new

--- chat/urls.py
+++ chat/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'chat'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('game/', views.game, name='game'),
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
+	path('', views.home, name='home'),
 	path('chat/', include('chat.urls')),
     path('admin/', admin.site.urls),
 ]
